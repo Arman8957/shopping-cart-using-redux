c44527898d466213701ec3837544f2ba6b89268e
Arman8957 <novicethought0@gmail.com>
2025-01-21 19:41:09 +0600

    footer added

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import { products } from '../constant/productList';
 import ProductCart from '../components/productCart';
+import Footer from '../components/Footer';
 
 const Home = () => {
     return (
         <div>
             <h1 className='text-3xl my-5'>List Products</h1>
             <div className='grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-5'>
-                 {products.map((product, key)=>
-                   <ProductCart key={key} data={product} />
-                 
-                 )}
+                {products.map((product, key) =>
+                    <ProductCart key={key} data={product} />
+
+                )}
 
             </div>
-            
+            <div className=" min-w-full" >
+                <Footer />
+            </div>
+
+
         </div>
     );
 }
